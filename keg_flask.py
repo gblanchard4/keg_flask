8d10b8a3d89a0f984e4790c3c30c690b11af5bf4
gblanchard4 <gblanchard4@gmail.com>
2014-04-14 18:53:22 -0500

    Figuring out the thermostat

--- keg_flask.py
+++ keg_flask.py
@@ -1,14 +1,14 @@
-from flask import Flask, render_template, request, jsonify
-import datetime
-import RPi.GPIO as GPIO
-from subprocess import check_output
+from flask import Flask, render_template, request, jsonify 
+import datetime 
+import RPi.GPIO as GPIO 
+from subprocess import check_output 
 import thermometer
+import thermostat
 
 app = Flask(__name__)
 
-GPIO.setmode(GPIO.BCM)
-
 @app.route("/")
+
 def Index():
 	return render_template("index.html", uptime=GetUptime())
 
@@ -22,17 +22,27 @@ def hello():
 	now = datetime.datetime.now()
 	timeString = now.strftime("%Y-%m-%d %H:%M")
 	tempString = thermometer.read_temp()
+	thermostat_status =  thermostat.get_state()
 	templateData = {
 		'title' : 'HELLO!',
 		'time' : timeString,
-		'temperature' : tempString
+		'temperature' : tempString,
+		'thermostat' : thermostat_status
 		}
 	return render_template('main.html', **templateData)
 
 @app.route("/_temperature")
-def _temperature():
+def _thermostat():
 	temp_string = thermometer.read_temp()
-	return jsonify(temperature = temp_string)
+	thermostat_status = thermostat.get_state()
+	return jsonify(temperature = temp_string, thermostat = thermostat_status)
 
 if __name__ == "__main__":
-	app.run(host='0.0.0.0', port=80, debug=True)
+	try:
+		app.run(host='0.0.0.0', port=80, debug=True)
+			
+	except KeyboardInterrupt:  
+		print "Cleaning up"
+	
+	finally:
+		GPIO.cleanup()
--- thermostat.py
+++ thermostat.py
@@ -3,20 +3,34 @@
 '''
 Gene Blanchard
 '''
-
+import thermometer
 import RPi.GPIO as GPIO
-import math
 
-def setup():
-	# PowerSwitch Tail II Pin
-	power_pin = 23
-	# Set the GPIO Mode to BCM
-	GPIO.setmode(GPIO.BCM)
-	# Setup the power pin
-	GPIO.setup(power_pin, GPIO.OUT)
+# PowerSwitch Tail II Pin
+power_pin = 23
+# Set the GPIO Mode to BCM
+GPIO.setmode(GPIO.BCM)
+# Setup the power pin, initialize off
+GPIO.setup(power_pin, GPIO.OUT, initial=GPIO.LOW)
 
-def getState():
-	return str(GPIO.input(power_pin))
+def thermostat(temperature):
 
-if __name__ == '__main__':
-	setup()
+	# Cast temperature to an int
+	temperature = int(float(temperature))
+	
+	# Optimal beer temperature is 50-55
+	beer_range = range(50,56) # 50-55F
+	
+	# If the temperature is too high
+	if temperature > beer_range[-1]:
+		# Turn the fridge on
+		GPIO.output(power_pin, True)
+	
+	# If the temperature is too low
+	if temperature < beer_range[0]:
+		# Turn the fidge off
+		GPIO.output(power_pin, False)
+	
+def get_state():
+	return str(GPIO.input(power_pin))	
+
