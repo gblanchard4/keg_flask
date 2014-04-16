1df264a2415d00ba5af4bcaf0d48a42facd20a91
gblanchard4 <gblanchard4@gmail.com>
2014-04-15 19:31:30 -0500

    working on a daemon

--- thermostat.py
+++ thermostat.py
@@ -6,15 +6,11 @@ Gene Blanchard
 import thermometer
 import RPi.GPIO as GPIO
 
-# PowerSwitch Tail II Pin
-power_pin = 23
-# Set the GPIO Mode to BCM
-GPIO.setmode(GPIO.BCM)
-# Setup the power pin, initialize off
-GPIO.setup(power_pin, GPIO.OUT, initial=GPIO.LOW)
+def thermostat():
 
-def thermostat(temperature):
-
+	# Get current temperature
+	thermometer.read_temp()
+	
 	# Cast temperature to an int
 	temperature = int(float(temperature))
 	
@@ -33,4 +29,28 @@ def thermostat(temperature):
 	
 def get_state():
 	return str(GPIO.input(power_pin))	
+
+def main():
+	
+	try:
+		# PowerSwitch Tail II Pin
+		power_pin = 23
+		# Set the GPIO Mode to BCM
+		GPIO.setmode(GPIO.BCM)
+		# Setup the power pin, initialize off
+		GPIO.setup(power_pin, GPIO.OUT, initial=GPIO.LOW)
+
+	except KeyboardInterrupt:
+	# here you put any code you want to run before the program   
+	# exits when you press CTRL+C  
+	print "\n", counter # print value of counter  
+	              except:  
+	                  # this catches ALL other exceptions including errors.  
+	                      # You won't get any error messages for debugging  
+	                          # so only use it once your code is working  
+	                              print "Other error or exception occurred!"  
+	                                
+	                                finally:  
+	                                    GPIO.cleanup() # this ensures a clean exit  
+	
 
